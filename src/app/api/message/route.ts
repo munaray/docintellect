1b7738e52b25c9fa3b50fb602ae823e703b38f79
munaray <munahdesigns@gmail.com>
2024-06-23 00:44:47 +0100

    fixing build error

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -62,7 +62,7 @@ export const POST = async (req: NextRequest) => {
                 fileId
             },
             orderBy: {
-                createAt: "asc"
+                createdAt: "asc"
             },
             take: 6
         })
