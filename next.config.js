895bb732bdc65f09cf63e389ca302d754a7d197a
munaray <munahdesigns@gmail.com>
2024-06-23 02:56:41 +0100

    update navbar

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+     async redirects() {
+    return [
+      {
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
+      },
+      {
+        source: '/sign-up',
+        destination: '/api/auth/register',
+        permanent: true,
+      },
+    ]
+  },
+
     env: {
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     },
@@ -7,7 +22,17 @@ const nextConfig = {
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false
         return config
-    },
+  },
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
-import {LoginLink, RegisterLink} from "@kinde-oss/kinde-auth-nextjs/server"
+import {getKindeServerSession, LoginLink, RegisterLink} from "@kinde-oss/kinde-auth-nextjs/server"
 import { ArrowRight } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
   return (
     <nav className='sticky inset-x-0 top-0 h-14 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
@@ -21,34 +25,56 @@ const Navbar = (props: Props) => {
 
           {/* Mobile Navigation */}
 
+
           <section className='hidden sm:flex items-center space-x-4'>
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({
-                  variant: 'ghost',
-                  size: "sm"
-                })}
-              >
-                Pricing
-              </Link>
-              <LoginLink
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm"
-                })}
-              >
-                Signin
-              </LoginLink>
-              <RegisterLink
-                className={buttonVariants({
-                  size: "sm",
-                })}
-              >
-                Get started
-                <ArrowRight className='w-5 h-5 ml-1.5'/>
-              </RegisterLink>
-            </>
+
+            {!user ? (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: "sm"
+                  })}
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm"
+                  })}
+                >
+                  Signin
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: "sm",
+                  })}
+                >
+                  Get started
+                  <ArrowRight className='w-5 h-5 ml-1.5' />
+                </RegisterLink>
+              </>) : (
+              <>
+                <Link
+                  href='/dashboard'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Dashboard
+                </Link>
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name ? "Your Account" : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ''}
+                  imageUrl={user.picture ?? ''}
+                />
+              </>
+            )}
           </section>
         </section>
       </MaxWidthWrapper>
